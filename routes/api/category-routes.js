24aa891691f54b1268b7e9c038e71e41bdf2855b
AKJ <kjajila@gmail.com>
2022-11-04 21:50:31 -0400

    add all delete routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,10 +43,10 @@ const { Category, Product } = require('../../models');
   // create a new category
   router.post('/', async (req, res) => {
     try {
-      const locationData = await LibraryCard.create({
-        reader_id: req.body.reader_id,
+      const categoryData = await Category.create({
+        category_id: req.body.category_id,
       });
-      res.status(200).json(locationData);
+      res.status(200).json(categoryData);
     } catch (err) {
       res.status(400).json(err);
     }
@@ -57,8 +57,25 @@ router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-});
+  router.delete('/:id', async (req, res) => {
+    try {
+      const categoryData = await Category.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (!categoryData) {
+        res.status(404).json({ message: 'No category found with that id!' });
+        return;
+      }
+  
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 
 module.exports = router;
